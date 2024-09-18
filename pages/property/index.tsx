1fad1ed2de54e812c10e43e73076f4adcf34e534
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-09-18 21:03:13 +0900

    Feat: L119

---
+++ pages/property/index.tsx
@@ -0,0 +1,9 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return <Container>Property List</Container>;
+};
+
+export default withLayoutBasic(PropertyList);
