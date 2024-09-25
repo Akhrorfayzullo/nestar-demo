c218182e5074b5eae3d9721e0d6013438864bb77
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-09-25 20:51:42 +0900

    fix: refactor pages components

--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -1,15 +1,13 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
 
-const Community: NextPage = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
-
+const CS: NextPage = () => {
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">Community</Stack>
+      <Stack className="container">CS</Stack>
     </div>
   );
 };
-
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,9 +1,11 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
-const PropertyList: NextPage = () => {
-  return <Container>Property List</Container>;
-};
+const PropertyList: NextPage = () => (
+  <div style={{ margin: "20px 0" }}>
+    <Stack className="container">PropertyList</Stack>
+  </div>
+);
 
 export default withLayoutBasic(PropertyList);
